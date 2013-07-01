9085f218cb998462d596f69d6eb29d1597da6ff1
wangyang <wangyang@wangyang-Latitude-E6530.(none)>
2013-07-01 15:02:10 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityStreamMongoid::Application.config.secret_token = 'ce641b5ebee56de447fcc9b9cf1d0f3eedd90e062e6972092ce9645302162a337a3cf91179cfc59269e3bc07a75b5718644a712e6f64eba85747f936e613ef22'
